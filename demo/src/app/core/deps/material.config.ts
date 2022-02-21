0e792fb488470271d5512bcddcc8a01070871187
Walter V. Faustine <waltervfaustine@gmail.com>
2022-02-21 12:46:03 +0300

    feat: 🎸 added navigation in demo app
    This commit will navigation in demo app

---
+++ demo/src/app/core/deps/material.config.ts
@@ -0,0 +1,20 @@
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+export const MaterialDeps = [
+  MatIconModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+  MatTabsModule,
+  MatSelectModule,
+  MatIconModule,
+  MatDialogModule,
+  MatButtonModule,
+];
