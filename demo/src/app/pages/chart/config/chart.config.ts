aa0f4c79e6ae39c07c92fc8c42e3c59c54b92e80
Walter V. Faustine <waltervfaustine@gmail.com>
2022-02-21 14:26:00 +0300

    feat: 🎸 improved navigation menu
    This commit will improve navigation menu

--- demo/src/app/pages/chart/config/chart.config.ts
+++ demo/src/app/pages/chart/config/chart.config.ts
@@ -3,32 +3,37 @@ import { Chart } from '../models/chart.model';
 export const chartConfigs: Chart[] = [
   {
     id: 'bar',
-    icon: 'bar_chart',
+    icon: '/assets/icons/bar.png',
     name: 'Bar Chart',
   },
   {
     id: 'column',
-    icon: 'bar_chart',
+    icon: '/assets/icons/column.png',
     name: 'Column Chart',
   },
   {
     id: 'pie',
-    icon: 'bar_chart',
+    icon: '/assets/icons/pie.png',
     name: 'Pie Chart',
   },
   {
     id: 'line',
-    icon: 'bar_chart',
+    icon: '/assets/icons/line.png',
     name: 'Line Chart',
   },
   {
     id: 'radar',
-    icon: 'bar_chart',
+    icon: '/assets/icons/radar.png',
     name: 'Radar Chart',
   },
   {
     id: 'dotted',
-    icon: 'bar_chart',
+    icon: '/assets/icons/dotted.png',
     name: 'Dotted Chart',
+  },
+  {
+    id: 'gauge',
+    icon: '/assets/icons/gauge.png',
+    name: 'Gauge Chart',
   },
 ];
