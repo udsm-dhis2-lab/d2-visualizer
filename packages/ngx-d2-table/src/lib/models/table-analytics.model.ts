4c29472e150c8754292522c0788dcb789202ff62
Walter V. Faustine <waltervfaustine@gmail.com>
2022-06-05 19:24:26 +0300

    feat: 🎸 improved normal table and scorecard component
    This commit will improve normal table and scorecard component

---
+++ packages/ngx-d2-table/src/lib/models/table-analytics.model.ts
@@ -0,0 +1,33 @@
+export interface TableAnalytics {
+    headers:     Header[];
+    metaData:    MetaData;
+    rows:        Array<string[]>;
+    width:       number;
+    height:      number;
+    headerWidth: number;
+}
+
+export interface Header {
+    name:      string;
+    column:    string;
+    valueType: string;
+    type:      string;
+    hidden:    boolean;
+    meta:      boolean;
+}
+
+export interface MetaData {
+    items:      { [key: string]: Item };
+    dimensions: Dimensions;
+}
+
+export interface Dimensions {
+    dx: string[];
+    pe: string[];
+    ou: string[];
+    co: any[];
+}
+
+export interface Item {
+    name: string;
+}
--- packages/ngx-d2-table/src/lib/ngx-d2-table.module.ts
+++ packages/ngx-d2-table/src/lib/ngx-d2-table.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { NgxD2TableComponent } from './ngx-d2-table.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+    MatListModule,
+    MatRadioModule,
+    MatButtonToggleModule,
+  ],
+  declarations: [NgxD2TableComponent],
+  exports: [NgxD2TableComponent],
 })
 export class NgxD2TableModule {}
