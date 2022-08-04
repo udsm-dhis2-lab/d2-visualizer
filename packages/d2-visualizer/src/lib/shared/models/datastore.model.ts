543d3c711f9888d2612db3540379b2fb603bf617
Walter V. Faustine <waltervfaustine@gmail.com>
2022-08-04 16:11:36 +0300

    feat: 🎸 added support for cascade models
    This commit will add support for cascade models

---
+++ packages/d2-visualizer/src/lib/shared/models/datastore.model.ts
@@ -0,0 +1,22 @@
+export interface DataStoreConfig {
+    id:         string;
+    xAlignment: string;
+    yAlignment: string;
+    zAlignment: string;
+    indicator:  Indicator;
+    categories: Category[];
+}
+
+export interface Category {
+    id:        string;
+    name:      string;
+    sortOrder: string;
+    target:    string;
+    color:     string;
+}
+
+export interface Indicator {
+    id:          string;
+    name:        string;
+    description: string;
+}
