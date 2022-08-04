cbeafa480da4a858ec05b61fb2fac2c340b8027e
Walter V. Faustine <waltervfaustine@gmail.com>
2022-08-04 16:12:14 +0300

    feat: 🎸 added support for cascade helper function
    This commit will add support for cascade helper function

---
+++ packages/d2-visualizer/src/lib/modules/chart/helpers/cascade.helper.ts
@@ -0,0 +1,88 @@
+import * as _ from 'lodash';
+import {
+    Category,
+    DataStoreConfig,
+} from '../../../shared/models/datastore.model';
+
+export const getTargetSeriesData = (dataStoreConfig: DataStoreConfig) => {
+    const categories: Category[] = _.orderBy(
+        _.map(
+            dataStoreConfig && dataStoreConfig.categories
+                ? dataStoreConfig.categories
+                : []
+        )
+    );
+    
+    return {
+        name: 'Targets',
+        stack: 1,
+        zIndex: 1,
+        pointPadding: 0,
+        dashStyle: 'dash',
+        borderColor: '#70C8BE',
+        borderWidth: 2,
+        dataLabels: [
+            {
+                align: 'center',
+                format: '{point.y}',
+                verticalAlign: 'top',
+                style: {
+                    color: 'black',
+                },
+            },
+        ],
+        data: _.map(categories, (category: Category) => {
+            return {
+                y: category && category ? +category.target : '',
+                color: '#ffffff',
+            };
+        }),
+    };
+};
+
+export const getAchievementSeriesData = (
+    chartObject: any,
+    dataStoreConfig: DataStoreConfig
+) => {
+    const series: any = _.head(
+        chartObject && chartObject.series ? chartObject.series : []
+    );
+    const seriesData = series && series.data ? _.orderBy(series.data, 'id') : [];
+
+    const categories =
+        dataStoreConfig && dataStoreConfig.categories
+            ? dataStoreConfig.categories
+            : [];
+
+    const categoriesObject = _.keyBy(categories);
+
+    return {
+        name: 'Achieved',
+        stack: 2,
+        zIndex: 2,
+        pointPadding: 0,
+        dataLabels: [
+            {
+                align: 'center',
+                format: '{point.y}',
+                verticalAlign: 'top',
+                style: {
+                    color: 'black',
+                },
+            },
+        ],
+        data: _.map(seriesData, (data: any) => {
+            return {
+                color:
+                    categoriesObject &&
+                        data &&
+                        data.id &&
+                        categoriesObject[data.id] &&
+                        categoriesObject[data.id].color
+                        ? categoriesObject[data.id].color
+                        : '#70C8BE',
+                y: data && data.y ? data.y : '',
+            };
+        }),
+    };
+};
